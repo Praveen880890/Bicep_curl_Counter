f878c6b95c9edb8c61ca77011fd5dd6ad1ee3129
Sureddy <praveen.rao.sureddy@accenture.com>
2025-02-24 10:19:10 +0530

    Adding model code and fully working app

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import os
 import PIL.Image, PIL.ImageTk
 import cv2
 import camera
-
+import model
 class App:
     def __init__(self):
         self.window = tk.Tk()
@@ -13,7 +13,7 @@ class App:
 
         self.extended = False
         self.contracted = False
-
+        self.model = model.Model() 
         self.last_prediction = 0
 
         self.counting_enabled = False
@@ -24,7 +24,7 @@ class App:
         self.window.attributes("-topmost",True)
         self.window.mainloop()
     def init_gui(self):
-        self.canvas = tk.Canva(self.window,width = self.camera.width,height = self.camera.height)
+        self.canvas = tk.Canvas(self.window,width = self.camera.width,height = self.camera.height)
         self.canvas.pack()
         self.btn_toggleauto = tk.Button(self.window,text ="Toggle Counting",command = self.counting_toggle)
         self.btn_toggleauto.pack(anchor = tk.CENTER,expand = True)
@@ -38,7 +38,7 @@ class App:
         self.train = tk.Button(self.window,text ="Train model",width = 50,command = lambda:self.model.train_model(self.counters))
         self.train.pack(anchor = tk.CENTER,expand = True)
 
-        self.reset = tk.Button(self.window,text ="Reset",width = 50,command = self.reset())
+        self.reset = tk.Button(self.window,text ="Reset",width = 50,command = self.reset)
         self.reset.pack(anchor = tk.CENTER,expand = True)
         self.counter_label = tk.Label(self.window,text = f"{self.rep_counter}")
         self.counter_label.config(font=("Arial",24))
@@ -46,8 +46,31 @@ class App:
 
     def counting_toggle(self):
         self.counting_enabled = not self.counting_enabled
+    
+    def update(self):
+        if self.counting_enabled:
+            self.predict()
+        if self.extended and self.contracted:
+            self.extended , self.contracted = False,False
+            self.rep_counter +=1
+        self.counter_label.config(text=f"{self.rep_counter}")
+        ret,frame = self.camera.get_frame()
+        if ret:
+            self.photo = PIL.ImageTk.PhotoImage(image = PIL.Image.fromarray(frame))
+            self.canvas.create_image(0,0,image= self.photo,anchor = tk.NW)
+        self.window.after(self.delay,self.update)
 
-        pass
+    def predict(self):
+        frame = self.camera.get_frame()
+        prediction = self.model.predict(frame)
+        if prediction != self.last_prediction:
+            if prediction ==1:
+                self.extended = True
+                self.last_prediction = 1
+            if prediction ==2:
+                self.contracted = True
+                self.last_prediction = 2
+
     def save_for_class(self,class_num):
         ret,frame = self.camera.get_frame()
         if not os.path.exists("1"):
@@ -56,9 +79,9 @@ class App:
             os.mkdir("2")
         cv2.imwrite(f"{class_num}/frame{self.counters[class_num-1]}.jpg",cv2.cvtColor(frame,cv2.COLOR_RGB2GRAY))
         img = PIL.Image.open(f"{class_num}/frame{self.counters[class_num-1]}.jpg")
-        img.thumbnail((150,150),PIL.Image.ANTIALTAS)
+        img.thumbnail((150,150),PIL.Image.ANTIALIAS)
         img.save(f"{class_num}/frame{self.counters[class_num-1]}.jpg")
         self.counters[class_num-1] +=1
     def reset(self):
         self.rep_counter = 0
-        pass
+        
